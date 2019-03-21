1e940d4864c2e435f9195f8ab263b1512805f050
Miguel E. Diganchi <migueldiganchi@gmail.com>
2019-03-21 02:30:48 -0300

    Validations implemented

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Note;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class NoteController extends Controller
 {
@@ -18,11 +19,15 @@ class NoteController extends Controller
     }
 
     /**
-     * Store a single note
+     * Creates a new note
      * 
      * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request) {
+        $errors = $this->validateNote($request);
+        if (!empty($errors)) {
+            return response()->json($errors, 422);
+        }
         $note = new Note();
         $note->title = $request->title;
         $note->content = $request->content;
@@ -37,6 +42,10 @@ class NoteController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id) {
+        $errors = $this->validateNote($request);
+        if (!empty($errors)) {
+            return response()->json($errors, 422);
+        }
         $note = Note::find($id);
         $note->title = $request->input('title');
         $note->content = $request->input('content');
@@ -46,7 +55,6 @@ class NoteController extends Controller
     }
 
     /**
-     * 
      * Removes a single note
      * 
      * @return \Illuminate\Http\JsonResponse
@@ -59,6 +67,33 @@ class NoteController extends Controller
             'status' => true,
             'message' => 'Note removed successfully'
         ]);
+    }
+
+    /**
+     * Validates if a note has a valid information to be saved
+     * 
+     * @return Array
+     * 
+     */
+    private function validateNote($request) {
+        $rules = [
+            'title' => 'required|min:3|max:42',
+            'content' => 'required|min:3|max:75'
+        ];
+        
+        $messages = [ 
+            'required' => 'This field is required', 
+            'min' => 'Too short', 
+            'max' => 'Too long' 
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return $validator->errors()->getMessages();
+        }
+
+        return [];
     }
 
 }
